cf7038a264d1305651acf0c0c1059330c53147c9
Guillaume <guillaume.briard@gmail.com>
2018-08-18 11:25:33 +0700

    Add shell for cron job

--- tests/TestCase/Shell/HelloShellTest.php
+++ tests/TestCase/Shell/HelloShellTest.php
@@ -1,11 +1,70 @@
 <?php
-namespace App\Shell;
-use Cake\Console\Shell;
+namespace App\Test\TestCase\Shell;
 
-class HelloShell extends Shell
+use App\Shell\HelloShell;
+use Cake\TestSuite\ConsoleIntegrationTestCase;
+
+/**
+ * App\Shell\HelloShell Test Case
+ */
+class HelloShellTest extends ConsoleIntegrationTestCase
 {
-    public function main()
+
+    /**
+     * ConsoleIo mock
+     *
+     * @var \Cake\Console\ConsoleIo|\PHPUnit_Framework_MockObject_MockObject
+     */
+    public $io;
+
+    /**
+     * Test subject
+     *
+     * @var \App\Shell\HelloShell
+     */
+    public $Hello;
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
     {
-        $this->out('Hello World');
+        parent::setUp();
+        $this->io = $this->getMockBuilder('Cake\Console\ConsoleIo')->getMock();
+        $this->Hello = new HelloShell($this->io);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->Hello);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test getOptionParser method
+     *
+     * @return void
+     */
+    public function testGetOptionParser()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test main method
+     *
+     * @return void
+     */
+    public function testMain()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
     }
 }
